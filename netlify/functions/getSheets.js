19220049ed715319513cb6804d192df6525d2943
BrunoHSL007 <brunohenrique.laier28@gmail.com>
2026-01-26 17:13:51 -0300

    Criação da tela de agenda

---
+++ netlify/functions/getSheets.js
@@ -0,0 +1,29 @@
+import { google } from "googleapis";
+
+export async function handler(event, context) {
+  try {
+    const auth = new google.auth.JWT({
+    email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+  });
+
+    const sheets = google.sheets({ version: "v4", auth });
+
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: "Eventos!A2:Z",
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res.data.values || []),
+    };
+  } catch (err) {
+    console.error("ERRO COMPLETO:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message || err }),
+    };
+  }
+}
